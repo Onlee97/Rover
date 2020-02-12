94e3754782c347e172fb01a26846ca7ec5a8c900
Onlee97 <lekhacduy97@gmail.com>
2020-02-11 20:38:42 -0500

    Elegoo Pi code

--- elegoo/run.py
+++ elegoo/run.py
@@ -5,16 +5,16 @@ import time
 import picamera
 
 #set GPIO numbering mode and define output pins
-l_foward = 31
-l_backward = 33
-r_forward = 37
-r_backward = 35
+forward = 31
+backward = 33
+left = 37
+right = 35
 
 GPIO.setmode(GPIO.BOARD)
-GPIO.setup(l_foward,GPIO.OUT)
-GPIO.setup(l_backward,GPIO.OUT)
-GPIO.setup(r_forward,GPIO.OUT)
-GPIO.setup(r_backward,GPIO.OUT)
+GPIO.setup(forward,GPIO.OUT)
+GPIO.setup(backward,GPIO.OUT)
+GPIO.setup(left,GPIO.OUT)
+GPIO.setup(right,GPIO.OUT)
 
 # Get the curses window, turn off echoing of keyboard to screen, turn on
 # instant (no waiting) key response, and use special values for cursor keys
@@ -23,10 +23,10 @@ curses.noecho()
 curses.cbreak()
 screen.keypad(True)
 #Initialize
-GPIO.output(l_foward,False)
-GPIO.output(r_forward,False)
-GPIO.output(l_backward,False)
-GPIO.output(r_backward,False)
+GPIO.output(forward,False)
+GPIO.output(left,False)
+GPIO.output(backward,False)
+GPIO.output(right,False)
 delaytime = 0.2
 while True:   
     char = screen.getch()
@@ -34,61 +34,45 @@ while True:
         break
     elif char == ord('w'):
         print("w")
-        GPIO.output(l_foward,False)
-        GPIO.output(r_forward,False)
-        GPIO.output(l_backward,False)
-        GPIO.output(r_backward,False)
+        GPIO.output(forward,True)
+        GPIO.output(left,False)
+        GPIO.output(backward,False)
+        GPIO.output(right,False)
         time.sleep(delaytime) 
-        GPIO.output(l_foward,True)
-        GPIO.output(r_forward,True)
-        GPIO.output(l_backward,False)
-        GPIO.output(r_backward,False)
 
     elif char == ord('s'):
         print("s")
-        GPIO.output(l_foward,False)
-        GPIO.output(r_forward,False)
-        GPIO.output(l_backward,False)
-        GPIO.output(r_backward,False)
+        GPIO.output(forward,False)
+        GPIO.output(left,False)
+        GPIO.output(backward,True)
+        GPIO.output(right,False)
         time.sleep(delaytime) 
-        GPIO.output(l_foward,False)
-        GPIO.output(r_forward,False)
-        GPIO.output(l_backward,True)
-        GPIO.output(r_backward,True)
 
     elif char == ord('d'):
         print("d")
-        GPIO.output(l_foward,False)
-        GPIO.output(r_forward,False)
-        GPIO.output(l_backward,False)
-        GPIO.output(r_backward,False)
+        GPIO.output(forward,False)
+        GPIO.output(left,False)
+        GPIO.output(backward,False)
+        GPIO.output(right,True)
         time.sleep(delaytime) 
-        GPIO.output(l_foward,True)
-        GPIO.output(r_forward,False)
-        GPIO.output(l_backward,False)
-        #GPIO.output(r_backward,True)
 
     elif char == ord('a'):
         print("a")
-        GPIO.output(l_foward,False)
-        GPIO.output(r_forward,False)
-        GPIO.output(l_backward,False)
-        GPIO.output(r_backward,False)
+        GPIO.output(forward,False)
+        GPIO.output(left,True)
+        GPIO.output(backward,False)
+        GPIO.output(right,False)
         time.sleep(delaytime) 
-        GPIO.output(l_foward,False)
-        GPIO.output(r_forward,True)
-        #GPIO.output(l_backward,True)
-        GPIO.output(r_backward,False)
 
     elif char == ord("e"): #Backspace
         print("STOP")
-        GPIO.output(l_foward,False)
-        GPIO.output(r_forward,False)
-        GPIO.output(l_backward,False)
-        GPIO.output(r_backward,False)
+        GPIO.output(forward,False)
+        GPIO.output(left,False)
+        GPIO.output(backward,False)
+        GPIO.output(right,False)
+        time.sleep(delaytime) 
          
-finally:
-    #Close down curses properly, inc turn echo back on!
-    curses.nocbreak(); screen.keypad(0); curses.echo()
-    curses.endwin()
-    GPIO.cleanup()
+#Close down curses properly, inc turn echo back on!
+curses.nocbreak(); screen.keypad(0); curses.echo()
+curses.endwin()
+GPIO.cleanup()
