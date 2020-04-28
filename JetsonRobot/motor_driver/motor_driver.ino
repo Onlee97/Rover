b787246cf04338bab0a5921b77397e0d3022fe39
Onlee97 <lekhacduy97@gmail.com>
2020-04-28 16:59:34 -0700

    MotorDriver reverse direction to the correct robot front

--- JetsonRobot/motor_driver/motor_driver.ino
+++ JetsonRobot/motor_driver/motor_driver.ino
@@ -9,11 +9,11 @@
 //  HIGH  HIGH  LOW   LOW   LOW   LOW   Car is stoped
 //  HIGH  HIGH  HIGH  HIGH  HIGH  HIGH  Car is stoped
 //  LOW   LOW   N/A   N/A   N/A   N/A   Car is stoped
-
+#include <string.h>
 
 //define L298n module IO Pin
-#define ENA 5 //Left
-#define ENB 6 //Right
+#define ENA 5 //Left enable
+#define ENB 6 //Right enable
 #define IN1 7
 #define IN2 8
 #define IN3 9
@@ -22,40 +22,40 @@
 void forward(){ 
 	digitalWrite(ENA,HIGH); //enable L298n A channel
 	digitalWrite(ENB,HIGH); //enable L298n B channel
-	digitalWrite(IN1,HIGH); //set IN1 hight level
-	digitalWrite(IN2,LOW);  //set IN2 low level
-	digitalWrite(IN3,LOW);  //set IN3 low level
-	digitalWrite(IN4,HIGH); //set IN4 hight level
+	digitalWrite(IN1,LOW); //set IN1 hight level
+	digitalWrite(IN2,HIGH);  //set IN2 low level
+	digitalWrite(IN3,HIGH);  //set IN3 low level
+	digitalWrite(IN4,LOW); //set IN4 hight level
 //	Serial.println("Forward");//send message to serial monitor`
 }
 
 void back(){
 	digitalWrite(ENA,HIGH);
 	digitalWrite(ENB,HIGH);
-	digitalWrite(IN1,LOW);
-	digitalWrite(IN2,HIGH);
-	digitalWrite(IN3,HIGH);
-	digitalWrite(IN4,LOW);
-//	Serial.println("Back");
-}
-
-void left(){
-	digitalWrite(ENA,LOW);
-	digitalWrite(ENB,HIGH);
-	digitalWrite(IN1,LOW);
-	digitalWrite(IN2,HIGH);
+	digitalWrite(IN1,HIGH);
+	digitalWrite(IN2,LOW);
 	digitalWrite(IN3,LOW);
-	digitalWrite(IN4,HIGH); 
-//	Serial.println("Left");
+	digitalWrite(IN4,HIGH);
+//	Serial.println("Back");
 }
 
 void right(){
 	digitalWrite(ENA,HIGH);
-	digitalWrite(ENB,LOW);
+	digitalWrite(ENB,HIGH);
 	digitalWrite(IN1,HIGH);
 	digitalWrite(IN2,LOW);
 	digitalWrite(IN3,HIGH);
-	digitalWrite(IN4,LOW);
+	digitalWrite(IN4,LOW); 
+//	Serial.println("Left");
+}
+
+void left(){
+	digitalWrite(ENA,HIGH);
+	digitalWrite(ENB,HIGH);
+	digitalWrite(IN1,LOW);
+	digitalWrite(IN2,HIGH);
+	digitalWrite(IN3,LOW);
+	digitalWrite(IN4,HIGH);
 //	Serial.println("Right");
 }
 
@@ -67,6 +67,18 @@ void stop(){
 	digitalWrite(IN3,LOW);
 	digitalWrite(IN4,LOW);
 //	Serial.println("Stop");
+}
+
+void leftWheel(int speed, bool isForward){
+	analogWrite(ENA, speed);
+	if (isForward){
+		digitalWrite(IN1,LOW); //set IN1 hight level
+		digitalWrite(IN2,HIGH);  //set IN2 low level
+	}
+	else {
+		digitalWrite(IN1,HIGH); //set IN1 hight level
+		digitalWrite(IN2,LOW);  //set IN2 low level
+	}
 }
 
 //before execute loop() function, 
@@ -87,6 +99,7 @@ enum direction {Forward, Backward, Left, Right, Stop};
 direction dir = Stop;
 
 void loop() {
+//	leftWheel(125, true);
 	if (Serial.available() > 0){
 		controlInput = Serial.read();		
 		switch(controlInput){
