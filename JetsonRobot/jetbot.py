6990266a5862ff9dc5fc1fe4a2a80e2323fa87a7
Duy Le <lekhacduy97@gmail.com>
2020-05-09 00:41:46 -0400

    Add jetbot class

--- JetsonRobot/jetbot.py
+++ JetsonRobot/jetbot.py
@@ -1,5 +1,26 @@
 import serial
 
 class jetbot:
-    def __init__(self):
-        print("Hello")
+    def __init__(self, _serialLink = "/dev/ttyACM0", _bandWidth = 9600):
+        self.serialLink = serial.Serial(_serialLink, _bandWidth)
+
+    def forward(self, speed = 1.0):
+        speed = int(speed*255)
+
+
+    def backward(self, speed = 1.0):
+        speed = int(speed*255)
+
+    def leftWheel(self, speed = 1.0):
+        speed = int(speed*255)
+        controlMsg = "L" + str(speed)
+        self.serialLink.write(controlMsg.encode())
+
+
+    def rightWheel(self, speed = 1.0):
+        speed = int(speed*255)
+        controlMsg = "R" + str(speed)
+        self.serialLink.write(controlMsg.encode())
+
+
+
