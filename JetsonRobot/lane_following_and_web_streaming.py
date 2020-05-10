b4fa6782fc901004fd148fe135eb3e2e6bee28ca
Duy Le <lekhacduy97@gmail.com>
2020-05-09 23:23:12 -0400

    lane following working, but need tuning and slow down serial rate

---
+++ JetsonRobot/lane_following_and_web_streaming.py
@@ -0,0 +1,142 @@
+import cv2
+import time
+import threading
+from flask import Response, Flask
+import sys
+import image_processing.lane_detection as ld
+from jetbot import Rover
+
+# Image frame sent to the Flask object
+global video_frame
+video_frame = None
+
+# Use locks for thread-safe viewing of frames in multiple browsers
+global thread_lock 
+thread_lock = threading.Lock()
+
+# Create global robot object
+global robot
+robot = Rover()
+
+# Create the Flask object for the application
+app = Flask(__name__)
+
+def gstreamer_pipeline(
+    #capture_width=1280,
+    #capture_height=720,
+    #display_width=1280,
+    #display_height=720,
+    #framerate=60,
+    #flip_method=0,
+
+    capture_width=320,
+    capture_height=180,
+    display_width=320,
+    display_height=180,
+    framerate=30,
+    flip_method=0,
+):
+    return (
+        "nvarguscamerasrc ! "
+        "video/x-raw(memory:NVMM), "
+        "width=(int)%d, height=(int)%d, "
+        "format=(string)NV12, framerate=(fraction)%d/1 ! "
+        "nvvidconv flip-method=%d ! "
+        "video/x-raw, width=(int)%d, height=(int)%d, format=(string)BGRx ! "
+        "videoconvert ! "
+        "video/x-raw, format=(string)BGR ! appsink"
+        % (
+            capture_width,
+            capture_height,
+            framerate,
+            flip_method,
+            display_width,
+            display_height,
+        )
+    )
+
+def control_motor(distance = 0):
+    #print("control2")
+    global robot
+    threshold = 30
+    speed = 0.5
+    speedDif = speed*((2*abs(distance))/320)
+    left_speed = speed*1.5
+    right_speed = speed*1.5
+    if (distance < -threshold):  #turn right
+        left_speed = speed + speedDif
+        right_speed = speed
+    elif (distance > threshold): #turn Left
+        left_speed = speed
+        right_speed = speed + speedDif
+    #robot.left(left_speed)
+    robot.right(right_speed)
+    #robot.right(0.5)
+    print("Distance: ", distance, "left: ", left_speed, "right: ", right_speed)
+
+
+def captureFrames():
+    global video_frame, thread_lock
+    
+    print(gstreamer_pipeline(flip_method=0))
+    
+    # Video capturing from OpenCV
+    video_capture = cv2.VideoCapture(gstreamer_pipeline(flip_method=0), cv2.CAP_GSTREAMER)
+    while True and video_capture.isOpened():
+        return_key, frame = video_capture.read()
+        if not return_key:
+            break
+        
+        #control_motor()
+        #Process image
+        try:
+            frame, distance = ld.detect_lane(frame)
+            print("control")
+            control_motor(distance)
+        except:
+            print("None Detected")
+            # Create a copy of the frame and store it in the global variable,
+            # with thread safe access
+        with thread_lock:
+            video_frame = frame.copy()
+        cv2.imshow('lane', frame)
+        key = cv2.waitKey(30) & 0xff
+        if key == 27:
+            break
+    cv2.destroyAllWindows()
+    video_capture.release()
+        
+def encodeFrame():
+    global thread_lock
+    while True:
+        # Acquire thread_lock to access the global video_frame object
+        with thread_lock:
+            global video_frame
+            if video_frame is None:
+                continue
+            return_key, encoded_image = cv2.imencode(".jpg", video_frame)
+            if not return_key:
+                continue
+
+        # Output image as a byte array
+        yield(b'--frame\r\n' b'Content-Type: image/jpeg\r\n\r\n' + 
+            bytearray(encoded_image) + b'\r\n')
+
+@app.route("/")
+def streamFrames():
+    return Response(encodeFrame(), mimetype = "multipart/x-mixed-replace; boundary=frame")
+
+# check to see if this is the main thread of execution
+if __name__ == '__main__':
+
+    # Create a thread and attach the method that captures the image frames, to it
+    process_thread = threading.Thread(target=captureFrames)
+    process_thread.daemon = True
+
+    # Start the thread
+    process_thread.start()
+
+    # start the Flask Web Application
+    # While it can be run on any feasible IP, IP = 0.0.0.0 renders the web app on
+    # the host machine's localhost and is discoverable by other machines on the same network 
+    app.run("0.0.0.0", port="8000")
